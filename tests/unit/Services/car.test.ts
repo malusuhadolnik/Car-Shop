4350bce2bde1a2501bc18ff6af78629bb057d8b6
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2023-04-27 14:54:09 -0300

    correção erros de lint

--- tests/unit/Services/car.test.ts
+++ tests/unit/Services/car.test.ts
@@ -6,61 +6,55 @@ import CarService from '../../../src/Services/CarService';
 import Car from '../../../src/Domains/Car';
 
 const carInput: ICar = {
-  model: "Marea",
+  model: 'Marea',
   year: 2002,
-  color: "Black",
+  color: 'Black',
   status: true,
   buyValue: 15.990,
   doorsQty: 4,
-  seatsQty: 5
+  seatsQty: 5,
 };
 const expectedResult: Car = new Car({
-  id: "6348513f34c397abcad040b2",
-  model: "Marea",
+  id: '6348513f34c397abcad040b2',
+  model: 'Marea',
   year: 2002,
-  color: "Black",
+  color: 'Black',
   status: true,
   buyValue: 15.990,
   doorsQty: 4,
-  seatsQty: 5
+  seatsQty: 5,
 });
 
 const expectedCarList: ICar[] = [
-    {
-      "id": "634852326b35b59438fbea2f",
-      "model": "Marea",
-      "year": 2002,
-      "color": "Black",
-      "status": true,
-      "buyValue": 15.99,
-      "doorsQty": 4,
-      "seatsQty": 5
-    },
-    {
-      "id": "634852326b35b59438fbea31",
-      "model": "Tempra",
-      "year": 1995,
-      "color": "Black",
-      "status": false,
-      "buyValue": 39,
-      "doorsQty": 2,
-      "seatsQty": 5
-    }
-  ]
+  {
+    id: '634852326b35b59438fbea2f',
+    model: 'Marea',
+    year: 2002,
+    color: 'Black',
+    status: true,
+    buyValue: 15.99,
+    doorsQty: 4,
+    seatsQty: 5,
+  },
+  {
+    id: '634852326b35b59438fbea31',
+    model: 'Tempra',
+    year: 1995,
+    color: 'Black',
+    status: false,
+    buyValue: 39,
+    doorsQty: 2,
+    seatsQty: 5,
+  },
+];
 
-describe('Deve criar um novo registro de carro', function () {
+describe('Testa o CRUD dos carros', function () {
   it('Deve criar um novo carro com sucesso', async function () {
-  // Arrange: criando mocks!
-  sinon.stub(Model, 'create').resolves(expectedResult);
-  // Act
-  const service = new CarService();
-  const result = await service.createNewCar(carInput);
-  // Assert
-  expect(result).to.be.deep.equal(expectedResult);
+    sinon.stub(Model, 'create').resolves(expectedResult);
+    const service = new CarService();
+    const result = await service.createNewCar(carInput);
+    expect(result).to.be.deep.equal(expectedResult);
   });
-});
-
-describe('Deve listar todos os carros', function () {
   it('Deve listar todos os carros com sucesso', async function () {
     // Arrange
     sinon.stub(Model, 'find').resolves(expectedCarList);
@@ -70,9 +64,6 @@ describe('Deve listar todos os carros', function () {
     // Assert
     expect(result).to.be.deep.equal(expectedCarList);
   });
-});
-
-describe('Deve retornar o carro de id correspondente', function () {
   it('Deve retornar um carro com sucesso', async function () {
     sinon.stub(Model, 'findOne').resolves(expectedResult);
     const service = new CarService();
