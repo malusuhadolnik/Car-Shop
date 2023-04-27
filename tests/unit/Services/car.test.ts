3a0573962a831de3cb816838e913c8b632d62ad3
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2023-04-27 14:39:21 -0300

    testes unitários para os métodos de criação e listagem de carros

---
+++ tests/unit/Services/car.test.ts
@@ -0,0 +1,103 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+import { Model } from 'mongoose';
+import ICar from '../../../src/Interfaces/ICar';
+import CarService from '../../../src/Services/CarService';
+import Car from '../../../src/Domains/Car';
+
+const carInput: ICar = {
+  model: "Marea",
+  year: 2002,
+  color: "Black",
+  status: true,
+  buyValue: 15.990,
+  doorsQty: 4,
+  seatsQty: 5
+};
+const expectedResult: Car = new Car({
+  id: "6348513f34c397abcad040b2",
+  model: "Marea",
+  year: 2002,
+  color: "Black",
+  status: true,
+  buyValue: 15.990,
+  doorsQty: 4,
+  seatsQty: 5
+});
+
+const expectedCarList: ICar[] = [
+    {
+      "id": "634852326b35b59438fbea2f",
+      "model": "Marea",
+      "year": 2002,
+      "color": "Black",
+      "status": true,
+      "buyValue": 15.99,
+      "doorsQty": 4,
+      "seatsQty": 5
+    },
+    {
+      "id": "634852326b35b59438fbea31",
+      "model": "Tempra",
+      "year": 1995,
+      "color": "Black",
+      "status": false,
+      "buyValue": 39,
+      "doorsQty": 2,
+      "seatsQty": 5
+    }
+  ]
+
+describe('Deve criar um novo registro de carro', function () {
+  it('Deve criar um novo carro com sucesso', async function () {
+  // Arrange: criando mocks!
+  sinon.stub(Model, 'create').resolves(expectedResult);
+  // Act
+  const service = new CarService();
+  const result = await service.createNewCar(carInput);
+  // Assert
+  expect(result).to.be.deep.equal(expectedResult);
+  });
+});
+
+describe('Deve listar todos os carros', function () {
+  it('Deve listar todos os carros com sucesso', async function () {
+    // Arrange
+    sinon.stub(Model, 'find').resolves(expectedCarList);
+    // Act
+    const service = new CarService();
+    const result = await service.listCars();
+    // Assert
+    expect(result).to.be.deep.equal(expectedCarList);
+  });
+});
+
+describe('Deve retornar o carro de id correspondente', function () {
+  it('Deve retornar um carro com sucesso', async function () {
+    sinon.stub(Model, 'findOne').resolves(expectedResult);
+    const service = new CarService();
+    const result = await service.getCarById('6348513f34c397abcad040b2');
+    expect(result).to.be.deep.equal(expectedResult);
+  });
+  it('Deve retornar um erro se a id passada for inválida', async function () {
+    sinon.stub(Model, 'findOne').resolves({});
+    try {
+      const service = new CarService();
+      await service.getCarById('invalid_id');
+    } catch (error) {
+      expect((error as Error).message).to.be.equal('Invalid mongo id');
+    }
+  });
+  it('Deve retornar um erro se não houver carro com a id passada', async function () {
+    sinon.stub(Model, 'findOne').resolves({});
+    try {
+      const service = new CarService();
+      await service.getCarById('6348513f34c397abcaXXX666');
+    } catch (error) {
+      expect((error as Error).message).to.be.equal('Car not found');
+    }
+  });
+  afterEach(function () {
+    sinon.restore();
+  });  
+});
